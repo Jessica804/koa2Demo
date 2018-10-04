903f0db6eccb76bf26290711992557ee65d400e9
guhy1 <guhy1@lenovo.com>
2018-10-04 11:36:27 +0800

    修改了gitignore,忽略七牛云的配置文件

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,8 @@
 module.exports ={
    "qiniu":{
-      //...
+       "bucket":'blogimg',
+       "video":'http://ozqubi472.bkt.clouddn.com/',
+       "AK":'gd52HNE5zADMOskpxcKCGt5mI3Rdqx02AT3Pym-i',
+       "SK":'NRBhHJVhNKWcykmfHjx4-JtvybSJ_puD9Vzd0u_h'
    }
 }
