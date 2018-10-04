30d75aa6e67682ea67222599b5414c4b33c1cbe2
guhy1 <guhy1@lenovo.com>
2018-10-04 11:41:04 +0800

    update .gitignore

--- server/config/index.js
+++
@@ -1,8 +0,0 @@
-module.exports ={
-   "qiniu":{
-       "bucket":'blogimg',
-       "video":'http://ozqubi472.bkt.clouddn.com/',
-       "AK":'gd52HNE5zADMOskpxcKCGt5mI3Rdqx02AT3Pym-i',
-       "SK":'NRBhHJVhNKWcykmfHjx4-JtvybSJ_puD9Vzd0u_h'
-   }
-}
